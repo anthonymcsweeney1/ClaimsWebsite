861d34905f2fa4c3d5bbe730470756f787632864
anthonymcsweeney1 <32956943+anthonymcsweeney1@users.noreply.github.com>
2020-11-11 13:19:26 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,16 @@
-<?php
-// PHP Data Objects(PDO) Sample Code:
-try {
-    $conn = new PDO("sqlsrv:server = tcp:databaseclaims.database.windows.net,1433; Database = ClaimsSite", "azureBIS", "{your_password_here}");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
-}
 
-// SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "azureBIS", "pwd" => "{your_password_here}", "Database" => "ClaimsSite", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:databaseclaims.database.windows.net,1433";
-$conn = sqlsrv_connect($serverName, $connectionInfo);
+
+<?php
+$servername = "tcp:databaseclaims.database.windows.net,1433";
+$username = "azureBIS";
+$password = "LokiKovex3!";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
 ?>

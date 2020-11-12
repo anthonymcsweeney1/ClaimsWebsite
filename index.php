a4f1ad00877350bff03d48bacea917a59208de5d
anthonymcsweeney1 <32956943+anthonymcsweeney1@users.noreply.github.com>
2020-11-12 13:35:10 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,24 @@
-<?php 
-session_save_path("ClaimsWebsite/sess_save");
-session_start();
-/*Start the Session Process*/
-echo $_SESSION["session_name"];
-/*Output : session value */
-?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+     <form action="login.php" method="post">
+     	<h2>LOGIN</h2>
+     	<?php if (isset($_GET['error'])) { ?>
+     		<p class="error"><?php echo $_GET['error']; ?></p>
+     	<?php } ?>
+     	<label>Username</label>
+     	<input type="text" name="uname" placeholder="Username"><br>
+
+     	<label>Password</label>
+     	<input type="password" name="password" placeholder="Password"><br>
+
+     	<button type="submit">Login</button>
+   
+	     <a href="Admin.php">SS</a>
+     </form>
+</body>
+</html>
